58e1004edad5e18d91c2bcf3f704c9dd42661387
sittisin <sittisin@dtac.co.th>
2015-11-23 12:03:18 +0700

    session2:bind ng-app, ng-controller to html

--- 2_form_and_controller/main.js
+++ 2_form_and_controller/main.js
@@ -1,1 +1,14 @@
+var myApp = angular.module("myApp", []);
 
+myApp.controller("FormController", function($scope){
+	$scope.formData = { 
+		name: "sittisin",
+		email: "sittisin@dtac.co.th"
+	};
+
+	$scope.register = function() {
+		alert("registered"+$scope.formData.name);
+	};
+
+
+})
