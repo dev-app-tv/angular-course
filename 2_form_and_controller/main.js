664267b8186e31ba355afab1f8f14f1291ea4796
sittisin <sittisin@dtac.co.th>
2015-11-23 12:21:40 +0700

    session2:reccommend practice to create controller and module

--- 2_form_and_controller/main.js
+++ 2_form_and_controller/main.js
@@ -1,14 +1,17 @@
-var myApp = angular.module("myApp", []);
+// Set angular module
+angular.module("myApp", []);
 
-myApp.controller("FormController", function($scope){
-	$scope.formData = { 
-		name: "sittisin",
-		email: "sittisin@dtac.co.th"
-	};
+// get controller
+angular.module("myApp")
+	.controller("FormController", function($scope) {
+		$scope.formData = {
+			name: "sittisin",
+			email: "sittisin@dtac.co.th"
+		};
 
-	$scope.register = function() {
-		alert("registered"+$scope.formData.name);
-	};
+		$scope.register = function() {
+			alert("registered" + $scope.formData.name);
+		};
 
 
-})
+	})
