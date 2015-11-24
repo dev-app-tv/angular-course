875f5b827c6c0d709c50b01f0f71e10d1c01f7a9
sittisin <sittisin@dtac.co.th>
2015-11-24 18:25:34 +0700

    byBlog:seperate 2 page, 2 controller

--- 14_promise/main.js
+++ 14_promise/main.js
@@ -11,46 +11,63 @@ app.controller('PromiseController', function ($timeout, $scope, $q) {
 
   var customer = kfcOfficer.promise;
 
-  customer.then(eatChicken, cancelOrder);
+  // customer.then(eatChicken, cancelOrder);
+  customer.then(eatChicken).then(changeOrder, cancelOrder);
 
   $scope.stop = function() {
     clearTimeout(order);
     kfcOfficer.reject("cancel order");
   };
 
+  $scope.change = function() {
+    clearTimeout(order);
+    kfcOfficer.reject("change order");
+  };
+
+  function changeOrder(data) {
+    console.info("customer: change order");
+  }
+
   function eatChicken(data){
       console.info("customer:yummy.");
   }
 
   function cancelOrder(data){
-      console.error("customer: "+data);
+    console.log("in cancelOrder");
+    if (data == "change order") {
+      return "wait change order";
+    } else {
+      console.info("customer: ...")
+      throw "customer: "+data;
+      // console.error("customer: "+data);
     }
+  }
 
 });
 
-angular.module('myApp').controller('ModalController', function($scope, $uibModal) {
-	$scope.open = function() {
-		var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: 500,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-	}
-});
+// angular.module('myApp').controller('ModalController', function($scope, $uibModal) {
+// 	$scope.open = function() {
+// 		var modalInstance = $uibModal.open({
+//       animation: $scope.animationsEnabled,
+//       templateUrl: 'myModalContent.html',
+//       controller: 'ModalInstanceCtrl',
+//       size: 500,
+//       resolve: {
+//         items: function () {
+//           return $scope.items;
+//         }
+//       }
+//     });
+// 	}
+// });
 
-angular.module('myApp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+// angular.module('myApp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
 
-  $scope.ok = function () {
-    $uibModalInstance.close('ok');
-  };
+//   $scope.ok = function () {
+//     $uibModalInstance.close('ok');
+//   };
 
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-});
+//   $scope.cancel = function () {
+//     $uibModalInstance.dismiss('cancel');
+//   };
+// });
--- myBlog/main.js
+++ myBlog/main.js
@@ -1,17 +1,38 @@
+var app = angular.module('myApp', ["ui.router"]);
 
-var app = angular.module('myApp', []);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "index.list.html",
+      controller: "ListBlogsController"
+    })
+    .state('add', {
+      url: "/add",
+      templateUrl: "index.add.html",
+      controller: "BlogsController"
+    })
+
+  $urlRouterProvider.otherwise('/home');
+})
 
 app.controller('BlogsController', function ($scope) {
-  $scope.contents = [];
-  $scope.limitRow = 4;
-  this.data = { blog: {
-      title: "t",
-      content: "r",
-      author: "a",
-      postDate: ""
-    }
-  };
-  $scope.data = this.data;
+  // $scope.contents = [{
+  //     title: "title",
+  //     content: "content",
+  //     author: "author",
+  //     postDate: ""
+  //   }];
+  // $scope.limitRow = 4;
+  // this.data = { blog: {
+  //     title: "title",
+  //     content: "content",
+  //     author: "author",
+  //     postDate: ""
+  //   }
+  // };
+  // $scope.data = this.data;
 
   $scope.postBlog = function(dat) {
     var n = new Date();
@@ -36,7 +57,47 @@ app.controller('BlogsController', function ($scope) {
     };
   };
 
-
 });
 
+app.controller('ListBlogsController', function ($scope) {
+  $scope.contents = [{
+      title: "title",
+      content: "content",
+      author: "author",
+      postDate: ""
+    }];
+  $scope.limitRow = 4;
+  this.data = { blog: {
+      title: "title",
+      content: "content",
+      author: "author",
+      postDate: ""
+    }
+  };
+  $scope.data = this.data;
+
+  // $scope.postBlog = function(dat) {
+  //   var n = new Date();
+  //   dat.postDate = n;
+  //   dat.preview = dat.content.substring(0,10)+"...";
+
+  //   var b = {
+  //     title: dat.title,
+  //     content: dat.content,
+  //     preview: dat.preview,
+  //     author: dat.author,
+  //     postDate: dat.postDate
+  //   }
+  //   this.contents.push(b);
+  //   this.data = {
+  //     blog: {
+  //       title: "title",
+  //       content: "content",
+  //       author: "author",
+  //       postDate: ""
+  //     }
+  //   };
+  // };
+
+});
 
