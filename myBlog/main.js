19c82a8cee39cd0e8276df14f7c77a4e171cb677
sittisin <sittisin@dtac.co.th>
2015-11-24 22:16:51 +0700

    add edit page

--- myBlog/main.js
+++ myBlog/main.js
@@ -1,103 +1,96 @@
 var app = angular.module('myApp', ["ui.router"]);
 
-
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: "/home",
-      templateUrl: "index.list.html",
+      templateUrl: "list.html",
       controller: "ListBlogsController"
     })
     .state('add', {
       url: "/add",
-      templateUrl: "index.add.html",
-      controller: "BlogsController"
+      templateUrl: "add.html"
+    })
+    .state('edit', {
+      url: "/edit",
+      templateUrl: "edit.html"
     })
 
   $urlRouterProvider.otherwise('/home');
 })
 
-app.controller('BlogsController', function ($scope) {
-  // $scope.contents = [{
-  //     title: "title",
-  //     content: "content",
-  //     author: "author",
-  //     postDate: ""
-  //   }];
-  // $scope.limitRow = 4;
-  // this.data = { blog: {
-  //     title: "title",
-  //     content: "content",
-  //     author: "author",
-  //     postDate: ""
-  //   }
-  // };
-  // $scope.data = this.data;
+app.service('BlogService',function() {
+  this.contents = [];
+
+  this.getContents = function() {
+    return this.contents;
+  }
+
+  this.add = function(data) {
+    this.contents.push(data);
+  }
+
+  this.select = function(data) {
+    this.selectecItem = data;
+  };
+
+  this.get = function() {
+    return this.selectecItem;
+  }
+
+});
+
+app.controller('BlogsController', function($scope, BlogService, $location) {
+  var init = {
+      title: "book of ",
+      content: "content on ",
+      author: "author ",
+      postDate: ""
+  };
+
+  this.data = init;
 
   $scope.postBlog = function(dat) {
-    var n = new Date();
-    dat.postDate = n;
-    dat.preview = dat.content.substring(0,10)+"...";
+    var blog = createBlog(dat);
+    BlogService.add(blog);
+    $location.path("/home");
+  };
 
-    var b = {
-      title: dat.title,
-      content: dat.content,
+  function createBlog(dat) {
+    var now = new Date();
+    dat.postDate = now;
+    dat.preview = dat.content.substring(0, 10) + "...";
+    return {
+      title: dat.title + now,
+      content: dat.content + now,
       preview: dat.preview,
-      author: dat.author,
+      author: dat.author + now,
       postDate: dat.postDate
-    }
-    this.contents.push(b);
-    this.data = {
-      blog: {
-        title: "title",
-        content: "content",
-        author: "author",
-        postDate: ""
-      }
     };
-  };
+  }
 
 });
 
-app.controller('ListBlogsController', function ($scope) {
-  $scope.contents = [{
-      title: "title",
-      content: "content",
-      author: "author",
-      postDate: ""
-    }];
-  $scope.limitRow = 4;
-  this.data = { blog: {
-      title: "title",
-      content: "content",
-      author: "author",
-      postDate: ""
-    }
-  };
-  $scope.data = this.data;
+app.controller('EditController', function($scope, BlogService, $location) {
+  $scope.data = { };
 
-  // $scope.postBlog = function(dat) {
-  //   var n = new Date();
-  //   dat.postDate = n;
-  //   dat.preview = dat.content.substring(0,10)+"...";
+  $scope.save = function(data) {
+    $location.path("/home");
+  }
 
-  //   var b = {
-  //     title: dat.title,
-  //     content: dat.content,
-  //     preview: dat.preview,
-  //     author: dat.author,
-  //     postDate: dat.postDate
-  //   }
-  //   this.contents.push(b);
-  //   this.data = {
-  //     blog: {
-  //       title: "title",
-  //       content: "content",
-  //       author: "author",
-  //       postDate: ""
-  //     }
-  //   };
-  // };
+  $scope.get = function() {
+    this.data = BlogService.get();
+  }
 
+  $scope.get();
 });
 
+
+app.controller('ListBlogsController', function($scope, BlogService) {
+  $scope.limitRow = 5;
+  $scope.contents = BlogService.getContents();
+
+  $scope.select = function(data) {
+    BlogService.select(data);
+  };
+});
