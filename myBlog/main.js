f34ecba60e49f796c7f556be29f174cd66c11bf7
sittisin <sittisin@dtac.co.th>
2015-11-24 09:19:18 +0700

    myBlog:apply limitTo,orderBy filter

--- myBlog/main.js
+++ myBlog/main.js
@@ -2,8 +2,8 @@
 var app = angular.module('myApp', []);
 
 app.controller('BlogsController', function ($scope) {
-	this.contents = [ ];
-
+  $scope.contents = [];
+  $scope.limitRow = 4;
   this.data = { blog: {
       title: "t",
       content: "r",
@@ -11,30 +11,31 @@ app.controller('BlogsController', function ($scope) {
       postDate: ""
     }
   };
+  $scope.data = this.data;
 
   $scope.postBlog = function(dat) {
     var n = new Date();
     dat.postDate = n;
+    dat.preview = dat.content.substring(0,10)+"...";
 
     var b = {
       title: dat.title,
       content: dat.content,
+      preview: dat.preview,
       author: dat.author,
       postDate: dat.postDate
     }
     this.contents.push(b);
-    this.data = { blog: {
-      title: "t",
-      content: "r",
-      author: "a",
-      postDate: ""
-    }
+    this.data = {
+      blog: {
+        title: "title",
+        content: "content",
+        author: "author",
+        postDate: ""
+      }
     };
   };
 
-
-  $scope.data = this.data;
-  $scope.contents = this.contents;
 
 });
 
