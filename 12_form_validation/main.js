40ec2c20abe06d8912ab92a6e680b2c78923ee48
sittisin <sittisin@dtac.co.th>
2015-11-25 11:45:19 +0700

    add sample validation

--- 12_form_validation/main.js
+++ 12_form_validation/main.js
@@ -1,7 +1,16 @@
 var app = angular.module('myApp', []);
 
 app.controller('RegisterController', function ($scope) {
-	$scope.register = function() {
-    console.log("register success");
+	$scope.register = function(isValid) {
+
+	$scope.isUnique = false;
+	$scope.myForm.name.$setValidity('unique', $scope.isUnique);
+
+	if (isValid) {
+		console.log("register success");
+	} else {
+		console.error("invalid format")
+	}
+    
   }
 });
